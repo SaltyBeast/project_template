1eff6ced4fd51374d87874f175e95f5e658fa8e0
Sasha Syniak <sashasyniak04@gmail.com>
2024-03-13 17:53:58 +0200

    read/write file functions definition

---
+++ app/io/input.py
@@ -0,0 +1,12 @@
+
+
+def console_input():
+    pass
+
+
+def read_file_builtin(file_name):
+    pass
+
+
+def read_file_pandas(file_name):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,12 @@
+
+
+def output_console():
+    pass
+
+
+def write_file_builtin():
+    pass
+
+
+def write_file_pandas():
+    pass
